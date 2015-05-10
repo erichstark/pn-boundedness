8fa3e9ad489a078fa4cae949a355bbf90b817252
Erich Stark <stark.erich72@gmail.com>
2015-05-10 19:03:54 +0200

    Hlavna trieda, kde sa vsetko spaja.

---
+++ java/src/com/erichstark/Main.java
@@ -0,0 +1,54 @@
+package com.erichstark;
+
+import com.erichstark.base.Arc;
+import com.erichstark.base.Place;
+import com.erichstark.base.Transition;
+import com.erichstark.parser.PflowParser;
+
+import java.util.List;
+
+/**
+ * Created by Erich on 10/05/15.
+ */
+public class Main {
+	public static void main(String[] args) {
+		List<Place> placeList;
+		List<Transition> transitionList;
+		List<Arc> arcList;
+
+		// parameter je cesta k suboru .pflow
+		PflowParser pflowParser = new PflowParser("/Users/Erich/Desktop/pflow/safe_ohr.pflow");
+
+
+		// zavola sa metoda triedy PflowParser a nasledne podla typu
+		// sa vyparsovane data ulozia do listu pre dalsie spracovanie do algoritmu
+		placeList = pflowParser.parsePlaces();
+		transitionList = pflowParser.parseTransitions();
+		arcList = pflowParser.parseArcs();
+
+
+		// Debug: vypisanie ulozenych objektov pre kontrolu
+		for (Place place : placeList) {
+			System.out.println("Place");
+			System.out.println("ID: " + place.getId());
+			System.out.println("Label: " + place.getLabel());
+			System.out.println("isStatic: " + place.isStatic());
+			System.out.println("Tokens: " + place.getTokens() + "\n");
+		}
+
+		for (Transition transition : transitionList) {
+			System.out.println("Transition");
+			System.out.println("ID: " + transition.getId());
+			System.out.println("Label: " + transition.getLabel() + "\n");
+		}
+
+		for (Arc arc : arcList) {
+			System.out.println("Arc");
+			System.out.println("Type: " + arc.getType());
+			System.out.println("SourceId: " + arc.getSourceId());
+			System.out.println("DestinationId: " + arc.getDestinationId());
+			System.out.println("Multiplicity: " + arc.getMultiplicity() + "\n");
+		}
+
+	}
+}
